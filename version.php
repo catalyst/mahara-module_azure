1b2413cfa8b42bbafcd43bb0902a409900e3ae04
Bevan Wishart <bevan@catalyst-au.net>
2018-01-22 16:28:02 +1100

    Initial commit

---
+++ version.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Version information.
+ *
+ * @package    mahara
+ * @subpackage module-azure
+ * @author     Bevan Wishart <bevan@catalyst-au.net>
+ * @copyright  Catalyst IT
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('INTERNAL') || die();
+$config = new stdClass();
+$config->version   = 2018010800;      // The current plugin version (Date: YYYYMMDDXX).
+$config->release   = '1.0.0';
